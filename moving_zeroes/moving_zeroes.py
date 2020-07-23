8e713bd77b2cad4915a779ace59ab7659c4fcfcf
alan5123 <alanparker5123@gmail.com>
2020-07-23 12:56:27 -0400

    two problems done

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -1,3 +1,4 @@
+  
 '''
 Input: a List of integers
 Returns: a List of integers
@@ -5,7 +6,20 @@ Returns: a List of integers
 def moving_zeroes(arr):
     # Your code here
 
-    pass
+    # the zeros need to be moved to the front of the passed array
+    # iterate over the arr
+    # is there a zero
+        # add it to a new array
+    # add the two arrays together, zero's in front
+    zero_arr = []
+    non_zeros = []
+    for i in arr:
+        if i == 0:
+            zero_arr.append(i)
+        else:
+            non_zeros.append(i)
+    moved_arr = non_zeros + zero_arr
+    return moved_arr
 
 
 if __name__ == '__main__':
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -5,7 +5,25 @@ Returns: an integer
 def single_number(arr):
     # Your code here
 
-    pass
+    # return a list
+    # needs to iterate over
+    # needs to check if there is a duplicate in that list
+    number = None
+    # iterate over the original array
+    for i in arr:
+        # check each number against the original array and see if it occurs again
+        index = i
+        occurences = 0
+        for num in arr:
+            if index == num:
+                occurences += 1
+        if occurences < 2:
+            number = index
+            return number
+            # if it does, increment the occurence
+        # if that number has more than one occurrence, keep going, otherwise return that number
+    return number
+
 
 
 if __name__ == '__main__':
